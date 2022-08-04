3443e8b858827a0bb9f76098e27bba6a978fa428
Dmitry <garredey123@gmail.com>
2022-08-04 14:06:12 +0300

    delete - 'fifth homework'

--- homework4/homework5/exercise4.py
+++
@@ -1,5 +0,0 @@
-n = int(input())
-for i in range(n + 1):
-    i = i ** 2
-    if i < n:
-        print(i)
--- homework4/homework5/exersice2.py
+++
@@ -1,7 +0,0 @@
-N = int(input())
-num = 10
-for i in range(1, N + 1):
-    if i == num:
-        num *= 10
-    if (i * i) % num == i:
-        print(i)
